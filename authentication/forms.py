6f5fa9e8789eff0cfc9348311697d506b4ee7045
Ivan Toulou <touloumbadjiani@gmail.com>
2018-12-03 11:39:50 +0000

    created forms for both login and register pages

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class loginForm(forms.Form):
     """
-    login form
+    login form for previous users
     """
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
  
+class registerForm(UserCreationForm):
+    """
+    Register form for new users
+    """
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    
+    class Meta:
+       model = User
+       fields = ['email', 'username', 'password1', 'password2']
