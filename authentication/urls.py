338dea18fb6176f14e9bd3e4fb402d54452b1014
Ivan Toulou <touloumbadjiani@gmail.com>
2018-12-03 11:45:47 +0000

    added logout url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from authentication.views import login
+from authentication.views import login, register, logout
 
 urlpatterns = [
         url(r'login/', login, name="login"),
+        url(r'logout/', logout, name="logout"),
+        url(r'register/', register, name="register"),
     ]
