5301f11ee49be26aecdde19779deec577cc8e633
Ivan Toulou <touloumbadjiani@gmail.com>
2018-12-03 11:46:46 +0000

    users are now authenticated when logging in and can logout

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from authentication.forms import loginForm
+from django.shortcuts import render, reverse, redirect
+from django.contrib import auth, messages
+from authentication.forms import loginForm, registerForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -12,5 +14,34 @@ def login(request):
     """
     user login page
     """
-    login_form = loginForm()
+    if request.method == "POST":
+        login_form = loginForm(request.POST)
+        
+        if login_form.is_valid():
+            user_logged_in = auth.authenticate(username=request.POST['username'],
+                                               password=request.POST['password'])
+            
+            if user_logged_in:
+                auth.login(user=user_logged_in, request=request)
+                messages.success(request, "Logged in as ")
+            else:
+                login_form.add_error(None, "username or password is incorrect")
+    else:
+        login_form = loginForm
     return render(request, 'login.html', {"login_form": login_form})
+
+def logout(request):
+    """
+    user logout
+    """
+    auth.logout(request)
+    messages.success(request, "Thank you for using Unicorn Attractor")
+    return redirect(reverse('index'))
+    
+
+def register(request):
+    """
+    register user page
+    """
+    register_form = registerForm()
+    return render(request, 'register.html', {"register_form": register_form})
