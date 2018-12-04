755907e8874cf0defd37bd6986c63f93fffd30c0
Ivan Toulou <touloumbadjiani@gmail.com>
2018-12-04 10:59:05 +0000

    user is directed to password reset page if forgotten password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from authentication.views import login, register, logout, profile
+from authentication import url_resets
 
 urlpatterns = [
-        url(r'login/', login, name="login"),
-        url(r'logout/', logout, name="logout"),
-        url(r'register/', register, name="register"),
-        url(r'profile/', profile, name="profile"),
+        url(r'^login/', login, name="login"),
+        url(r'^logout/', logout, name="logout"),
+        url(r'^register/', register, name="register"),
+        url(r'^profile/', profile, name="profile"),
+        url(r'^password-reset/', include(url_resets)),
     ]
