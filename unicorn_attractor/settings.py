25e05c00fe77504cc5d4cd6469f7ce22e7060532
Ivan Toulou <touloumbadjiani@gmail.com>
2019-01-23 14:31:46 +0000

    updated settings.py

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['unicorn-attractor-itoulou.c9users.io']
+ALLOWED_HOSTS = ['unicorn-attractor-itoulou.c9users.io', 'unicorn-attractor-tou.herokuapp.com']
 
 
 # Application definition
